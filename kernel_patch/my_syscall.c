90ecd946412c7e051f11ff966c8186f8af5125d8
Ghostrider_1218 <160344693+Anuragshrestha9768@users.noreply.github.com>
2025-08-24 16:08:54 +0545

    Update my_syscall.c

--- kernel_patch/my_syscall.c
+++ kernel_patch/my_syscall.c
@@ -1,1 +1,10 @@
 
+// kernel/my_syscall.c
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+
+SYSCALL_DEFINE1(my_syscall, int x)
+{
+    printk(KERN_INFO "my_syscall called with %d\n", x);
+    return x * 42;
+}
