9e879f563902f5fea91ebe54811f2e5ebac9ade2
Ghostrider_1218 <160344693+Anuragshrestha9768@users.noreply.github.com>
2025-08-24 16:14:08 +0545

    Update test_syscall.c

--- user_program/test_syscall.c
+++ user_program/test_syscall.c
@@ -1,1 +1,12 @@
 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#define __NR_my_syscall 548
+
+int main() {
+    int res = syscall(__NR_my_syscall, 5);
+    printf("Result: %d\n", res);
+    return 0;
+}
