7a583bf57d40d9134b979cc546f54a01efdb8c4e
Ghostrider_1218 <160344693+Anuragshrestha9768@users.noreply.github.com>
2025-08-24 16:10:54 +0545

    Delete my_syscall.c

--- my_syscall.c
+++
@@ -1,9 +0,0 @@
-// kernel/my_syscall.c
-#include <linux/kernel.h>
-#include <linux/syscalls.h>
-
-SYSCALL_DEFINE1(my_syscall, int x)
-{
-    printk(KERN_INFO "my_syscall called with %d\n", x);
-    return x * 42;
-}
